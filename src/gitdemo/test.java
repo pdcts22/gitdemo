bd82b0a74a7a17713c3d118313fef0074104e84d
pradnyaearnity <99770296+pradnyaearnity@users.noreply.github.com>
2022-06-17 07:16:45 -0700

    first commit

---
+++ src/gitdemo/test.java
@@ -0,0 +1,16 @@
+package gitdemo;
+
+public class test {
+	
+public static void main(String[] args) {
+	
+	String hungry="yes";
+	if(hungry=="yes")
+	System.out.println("eat");
+	else {
+		System.out.println("do work");
+	}
+	
+}
+
+}
